1da8621abd4f8cdeaed932fde8962711e73c81cc
milank90 <66497985+milank90@users.noreply.github.com>
2024-03-11 06:11:19 +0200

    new

--- app/fighters/[name]/test.jsx
+++ app/fighters/[name]/test.jsx
@@ -1,58 +1,58 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { db } from "@/app/DBinit";
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { db } from "@/app/DBinit";
 
 
-export default function FighterProfile() {
-    const router = useRouter();
-    const [fighterData, setFighterData] = useState(null);
-    const [loading, setLoading] = useState(true);
+// export default function FighterProfile() {
+//     const router = useRouter();
+//     const [fighterData, setFighterData] = useState(null);
+//     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const fightersSnapshot = await db.collection("Fighters").get();
-                const allEntries = fightersSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-                setFighterData(allEntries);
-                setLoading(false);
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             try {
+//                 const fightersSnapshot = await db.collection("Fighters").get();
+//                 const allEntries = fightersSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+//                 setFighterData(allEntries);
+//                 setLoading(false);
 
-                // If the name parameter exists, display the corresponding fighter profile
-                const { name } = router.query;
+//                 // If the name parameter exists, display the corresponding fighter profile
+//                 const { name } = router.query;
 
-                if (name) {
-                    displayFighterProfile(name, allEntries);
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-                setLoading(false);
-            }
-        };
+//                 if (name) {
+//                     displayFighterProfile(name, allEntries);
+//                 }
+//             } catch (error) {
+//                 console.error("Error fetching data:", error);
+//                 setLoading(false);
+//             }
+//         };
 
-        fetchData();
-    }, [router.query]);
+//         fetchData();
+//     }, [router.query]);
 
-    const displayFighterProfile = (fighterName, allEntries) => {
-        const fighterProfile = allEntries.find((entry) => entry.name === fighterName);
-        if (fighterProfile) {
-            // Display the fighter's profile on the profile page
-            document.getElementById("profileImage").src = fighterProfile.img1;
-            document.getElementById("profileName").textContent = `Name: ${fighterProfile.name}`;
-            document.getElementById("profileDataCenter").textContent = `Data Center/World: ${fighterProfile.world}`;
-            document.getElementById("profileRank").textContent = `Rank: #${fighterProfile.rank}`;
-            document.getElementById("profilePoints").textContent = `Points: ${fighterProfile.points}`;
-            document.getElementById("profileFights").textContent = `Fights: ${fighterProfile.fights}`;
-            document.getElementById("profileWins").textContent = `Wins: ${fighterProfile.wins}`;
-            document.getElementById("profileLosses").textContent = `Losses: ${fighterProfile.losses}`;
-            document.getElementById("profileDraws").textContent = `Draws: ${fighterProfile.draws}`;
-        } else {
-            console.error("Fighter not found:", fighterName);
-        }
-    };
+//     const displayFighterProfile = (fighterName, allEntries) => {
+//         const fighterProfile = allEntries.find((entry) => entry.name === fighterName);
+//         if (fighterProfile) {
+//             // Display the fighter's profile on the profile page
+//             document.getElementById("profileImage").src = fighterProfile.img1;
+//             document.getElementById("profileName").textContent = `Name: ${fighterProfile.name}`;
+//             document.getElementById("profileDataCenter").textContent = `Data Center/World: ${fighterProfile.world}`;
+//             document.getElementById("profileRank").textContent = `Rank: #${fighterProfile.rank}`;
+//             document.getElementById("profilePoints").textContent = `Points: ${fighterProfile.points}`;
+//             document.getElementById("profileFights").textContent = `Fights: ${fighterProfile.fights}`;
+//             document.getElementById("profileWins").textContent = `Wins: ${fighterProfile.wins}`;
+//             document.getElementById("profileLosses").textContent = `Losses: ${fighterProfile.losses}`;
+//             document.getElementById("profileDraws").textContent = `Draws: ${fighterProfile.draws}`;
+//         } else {
+//             console.error("Fighter not found:", fighterName);
+//         }
+//     };
 
-    if (!fighterData) {
-        return <p>Error fetching data. Please try again later.</p>;
-    }
+//     if (!fighterData) {
+//         return <p>Error fetching data. Please try again later.</p>;
+//     }
 
-    return <FighterData {...props} />;
-}
+//     return <FighterData {...props} />;
+// }
--- app/fighters/components/FightersCard test.jsx
+++ app/fighters/components/FightersCard test.jsx
@@ -1,47 +1,47 @@
-"use client";
-import React from "react";
-import { useRouter } from "next/navigation";
+// "use client";
+// import React from "react";
+// import { useRouter } from "next/navigation";
 
-export default function FightersCard(props) {
-    const router = useRouter();
+// export default function FightersCard(props) {
+//     const router = useRouter();
 
-    return (
-        <div
-            type="button"
-            style={{ height: "50px", border: "1px solid black", cursor: "pointer" }}
-            onClick={() => {
-                router.push(`/fighters/${props.name.replace(/\s+/g, ".")}`);
-            }}
-            className="fighter-card"
-        >
-            {props.img1 && (
-                <img
-                    src={props.img1}
-                    alt="Fighter Image"
-                    className="fighter-image"
-                />
-            )}
-            <p>{props.name}</p>
-            <div>
-                <div>
-                    <div>Rank</div>
-                    <div>Points</div>
-                    <div>Fights</div>
-                </div>
-                <div>
-                    <div>props.rank</div>
-                    <div>props.points</div>
-                    <div>props.totalfights</div>
-                </div>
-            </div>
-            <div>
-                <div>W {props.wins}</div>
-                <div>
-                    <span> - L - </span>
-                    <span>- {props.losses} -</span>
-                </div>
-                <div>D {props.draws}</div>
-            </div>
-        </div>
-    );
-}
+//     return (
+//         <div
+//             type="button"
+//             style={{ height: "50px", border: "1px solid black", cursor: "pointer" }}
+//             onClick={() => {
+//                 router.push(`/fighters/${props.name.replace(/\s+/g, ".")}`);
+//             }}
+//             className="fighter-card"
+//         >
+//             {props.img1 && (
+//                 <img
+//                     src={props.img1}
+//                     alt="Fighter Image"
+//                     className="fighter-image"
+//                 />
+//             )}
+//             <p>{props.name}</p>
+//             <div>
+//                 <div>
+//                     <div>Rank</div>
+//                     <div>Points</div>
+//                     <div>Fights</div>
+//                 </div>
+//                 <div>
+//                     <div>props.rank</div>
+//                     <div>props.points</div>
+//                     <div>props.totalfights</div>
+//                 </div>
+//             </div>
+//             <div>
+//                 <div>W {props.wins}</div>
+//                 <div>
+//                     <span> - L - </span>
+//                     <span>- {props.losses} -</span>
+//                 </div>
+//                 <div>D {props.draws}</div>
+//             </div>
+//         </div>
+//     );
+// }
